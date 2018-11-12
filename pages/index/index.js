cbadd80ab8c8bb9d7589dd99fbebe7a30215e64e
hanlei525 <190182347@qq.com>
2018-11-12 17:21:13 +0800

    1

---
+++ pages/index/index.js
@@ -0,0 +1,89 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
+    wx.request({
+      url: 'https://free-api.heweather.com/s6/weather/now?key=8ae6f0d768134091be43afd9f777ff5a&location=beijing',
+      success:function(e){
+        if(e.statusCode==200){
+          weather_now: e.data.HeWeather6[0].now.cond_txt
+        }
+      }
+    })
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+  onPullDownRefresh:function(e){
+    console.log(1)
+  },
+  onReachBottom:function(e){
+    console.log(2)
+  },
+  onShareAppMessage:function(res){
+    if (res.from === 'button') {
+      // 来自页面内转发按钮
+      console.log(res.target)
+    }
+    return {
+      title: 'HL_weather',
+      path: '/pages/index/index'
+    }
+  },
+  onTabItemTap(item) {
+    console.log(item.index)
+    console.log(item.pagePath)
+    console.log(item.text)
+  },
+  viewTap: function () {
+    console.log('view tap')
+  },
+  onShow: function () {
+    console.log(this.route)
+  }
+})
